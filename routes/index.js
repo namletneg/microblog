a7619ab7171d902244dec8e1f2ab57415a6276ee
namletneg <nameltneg@163.com>
2014-12-09 16:56:01 +0800

    实现了注册功能
    nodejs开发的博客，暂时实现了注册功能

---
+++ routes/index.js
@@ -0,0 +1,80 @@
+
+/*
+ * GET home page.
+ */
+var crypto = require('crypto'),
+    User = require('../models/user');
+module.exports = function(app){
+    app.get('/',function(req, res){
+        res.render('index',{title: '首页'});
+    });
+    app.get('/reg',function(req, res){
+        res.render('reg',{title: '用户注册'});
+    });
+    app.post('/reg',function(req, res){
+        //检验用户两次输入的口令是否一致
+        if(req.body['password-repeat'] != req.body['password']){
+            req.flash('error', '两次输入的密码不一致');
+            return res.redirect('/reg');
+        }
+        //生产密码的散列值
+        var md5 = crypto.createHash('md5'),
+            password = md5.update(req.body.password).digest('base64'),
+            newUser = new User({
+                name: req.body.username,
+                password: password
+            });
+        //检查用户名是否已经存在
+        User.get(newUser.name, function(err, user){
+            if(user){
+                err = 'Username already exists';
+            }
+            if(err){
+                req.flash('error', err);
+                return res.redirect('/reg');
+            }
+            //如果不存在则新增用户
+            newUser.save(function(err){
+                if(err){
+                    req.flash('error',err);
+                    return res.redirect('/reg');
+                }
+                req.session.user = newUser;
+                req.flash('success', '注册成功');
+                res.redirect('/');
+            });
+        });
+    });
+};
+
+/*exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+exports.user = function(req, res){
+
+};
+
+exports.post = function(req, res){
+
+};
+
+exports.reg = function(req, res){
+
+};
+
+exports.doReg = function(req, res){
+
+};
+
+exports.login = function(req, res){
+
+};
+
+exports.doLogin = function(req, res){
+
+};*/
+
+exports.logout = function(req, res){
+
+};
---
+++ settings.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2014/12/4.
+ */
+module.exports = {
+    cookieSecret: 'microblogfsw',
+    db: 'microblog',
+    host: 'localhost'
+};
