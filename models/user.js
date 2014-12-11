e5f04cf0819c80ee0157044e2f59a6807660c84d
namletneg <nameltneg@163.com>
2014-12-11 17:50:14 +0800

    添加post功能

--- models/user.js
+++ models/user.js
@@ -1,86 +1,64 @@
-/**
- * Created by Administrator on 2014/12/5.
- */
 var mongodb = require('./db');
-function User(user){
-    this.name = user.name;
-    this.password = user.password;
+
+function User(user) {
+	this.name = user.name;
+	this.password = user.password;
 }
+
 module.exports = User;
 
-User.prototype.save = function save(callback){
-    //存入mongodb 的文档
-    var user = {
-        name: this.name,
-        password: this.password
-    };
-    mongodb.open(function(err,db){
-        if(err){
-            return callback(err);
-        }
-        //读取users集合
-        db.collection('users',function(err, collection){
-            if(err){
-                mongodb.close();
-                return callback(err);
-            }
-            //为name属性添加索引
-            collection.ensureIndex('name', {unique: true});
-            //写入user文档
-            collection.insert(user, {safe: true}, function(err, user){
-                mongodb.close();
-                callback(null,user[0]);  //原文callback(err, user)有误
-            });
-        });
-    });
-};
-//读取用户信息，通过用户名
-User.get = function get(username, callback){
-    mongodb.open(function(err, db){
-        if(err){
-            return callback(err);
-        }
-        //读取users 集合
-        db.collection('users', function(err, collection){
-            if(err){
-                mongodb.close();
-                return callback(err);
-            }
-            //查找name属性为 username的文档
-            collection.findOne({name: username}, function(err, doc){
-                mongodb.close();
-                if(doc){
-                    //封装文档为 User对象
-                    var user = new User(doc);
-                    callback(err, user);
-                } else{
-                    callback(err, null);
-                }
-            });
-        });
-    });
+User.prototype.save = function save(callback) {
+	var user = {
+		name: this.name,
+		password: this.password
+	};
+	
+	mongodb.open(function(err, db) {
+		if (err) {
+		  return callback(err);
+		}
+
+		//获取users集合
+		db.collection('users', function(err, collection) {
+			if (err) {
+				mongodb.close();
+				return callback(err);
+			}
+
+			//为name属性添加索引
+			collection.ensureIndex('name', {unique: true});
+
+			//save
+			collection.insert(user, {safe: true}, function(err, user) {
+				mongodb.close();
+				callback(err, user);
+			});
+		});
+	});
 };
 
+User.get = function get(username, callback) {
+	mongodb.open(function(err, db) {
+		if (err) {
+			return callback(err);
+		}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		db.collection('users', function(err, collection) {
+			if (err) {
+				mongodb.close();
+				return callback(err);
+			}
+			
+			//find
+			collection.findOne({name: username}, function(err, doc) {
+				mongodb.close();
+				if (doc) {
+					var user = new User(doc);
+					callback(err, user);
+				} else {
+					callback(err, null);
+				}
+			});
+		});
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,18 @@
  * GET home page.
  */
 var crypto = require('crypto'),
-    User = require('../models/user');
+    User = require('../models/user'),
+    Post = require('../models/post');
 module.exports = function(app){
     app.get('/',function(req, res){
         res.render('index',{title: '首页'});
     });
+
+    //app.get('/reg', checkNoLogin);
     app.get('/reg',function(req, res){
         res.render('reg',{title: '用户注册'});
     });
+    //app.post('/reg', checkNoLogin);
     app.post('/reg',function(req, res){
         //检验用户两次输入的口令是否一致
         if(req.body['password-repeat'] != req.body['password']){
@@ -27,7 +31,7 @@ module.exports = function(app){
         //检查用户名是否已经存在
         User.get(newUser.name, function(err, user){
             if(user){
-                err = 'Username already exists';
+                err = '抱歉，您晚了一步，用户名已被注册！';
             }
             if(err){
                 req.flash('error', err);
@@ -41,11 +45,93 @@ module.exports = function(app){
                 }
                 req.session.user = newUser;
                 req.flash('success', '注册成功');
+                console.log(666);
                 res.redirect('/');
+                console.log(777);
+            });
+        });
+    });
+
+    //app.get('/login', checkNoLogin);
+    app.get('/login',function(req,res){
+        res.render('login',{title: '用户登录'});
+    });
+    //app.post('/login', checkNoLogin);
+    app.post('/login',function(req, res){
+        //生产密码的散列值
+        var md5 = crypto.createHash('md5'),
+            password = md5.update(req.body.password).digest('base64');
+
+        User.get(req.body.username, function(err, user){
+            if(!user){
+                req.flash('error', '用户不存在');
+                return res.redirect('/login');
+            }
+            if(user.password !== password){
+                req.flash('error', '用户密码不正确');
+                return res.redirect('/login');
+            }
+            req.session.user = user;
+            req.flash('success', '登录成功');
+            res.redirect('/');
+        });
+    });
+
+    //app.get('/logout', checkLogin);
+    app.get('/logout', function(req, res){
+        req.session.user = null;
+        req.flash('success','退出成功');
+        res.redirect('/');
+    });
+
+    //app.post('/post', checkLogin);
+    app.post('/post', function(req, res){
+        var currentUser = req.session.user,
+            post = new Post(currentUser.name, req.body.post);
+
+        post.save(function(err){
+            if(err){
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            req.flash('success', '发表成功');
+            res.redirect('/u/' + currentUser.name);
+        });
+    });
+    app.get('/u/:user', function(req, res){
+        User.get(req.params.user, function(err, user){
+            if(!user){
+                req.flash('error', '用户不存在');
+                return res.redirect('/');
+            }
+            Post.get(user.name, function(err, posts){
+                if(err){
+                    req.flash('error', err);
+                    return res.redirect('/');
+                }
+                res.render('user',{
+                    title: user.name,
+                    posts: posts
+                });
             });
         });
     });
 };
+
+function checkLogin(req, res, next){
+    if(!req.session.user){
+        req.flash('error', '未登录');
+        return res.redirect('/login');
+    }
+    next();
+}
+function checkNoLogin(req, res, next){
+    if(req.session.user){
+        req.flash('error', '已登录');
+        return res.redirect('/');
+    }
+    next();
+}
 
 /*exports.index = function(req, res){
   res.render('index', { title: 'Express' })
